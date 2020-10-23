b204d50dc1a9b63a60c2f8699782cafed57cba87
AlfaLucky <shiryaaa@mail.ru>
2020-10-24 00:58:14 +0300

    feat: added bracket
    https://github.com/Shastel/brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+let len = bracketsConfig.length;
+  let comparisor = "";
+  let count = 0;
+  let brackets = "";
+  while (count !== len) {
+    count = 0;
+    for (let i = 0; i < len; i++) {
+      brackets = bracketsConfig[i][0] + bracketsConfig[i][1];
+      comparisor = str.replace(brackets, "");
+      if (str === comparisor) {
+        count++;
+      }
+      str = comparisor;
+    }
+  }
+  return str.length === 0;
 }
